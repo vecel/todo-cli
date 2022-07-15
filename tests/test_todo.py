65e8211b6c2ab6ff5dfcffc99cd1c99736dfa2c9
Mateusz Karandys <mateusz.karandys@gmail.com>
2022-07-15 12:21:10 +0200

    Add version unit test

---
+++ tests/test_todo.py
@@ -0,0 +1,9 @@
+from typer.testing import CliRunner
+from todo import cli, __app_name__, __version__
+
+runner = CliRunner()
+
+def test_version():
+    result = runner.invoke(cli.app, ["--version"])
+    assert result.exit_code == 0
+    assert f"{__app_name__} v{__version__}" in result.stdout
