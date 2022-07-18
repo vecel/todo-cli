26906d0d4ad25a0e15c215ad6d7449b7fd6f68ae
Mateusz Karandys <mateusz.karandys@gmail.com>
2022-07-18 20:55:49 +0200

    Add init command

--- todo/cli.py
+++ todo/cli.py
@@ -1,5 +1,6 @@
 from typing import Optional
-from todo import __app_name__, __version__
+from todo import ERRORS, __app_name__, __version__, config, database
+from pathlib import Path
 import typer
 
 app = typer.Typer()
@@ -21,3 +22,31 @@ def main(
     )
 ) -> None:
     return
+
+@app.command()
+def init(
+    db_path: str = typer.Option(
+        str(database.DEFAULT_DB_PATH),
+        "--db-path",
+        "-db",
+    )
+) -> None:
+    """
+    Initialize to-do database with default PATH\n
+    If --db-path or -db is used, PATH given as an argument will be used
+    """
+    app_init_error = config.init_app(db_path)
+    if app_init_error:
+        typer.secho(
+            f"Creating config file failed with {ERRORS[app_init_error]}",
+            fg = typer.colors.RED
+        )
+        raise typer.Exit(1)
+    db_init_error = database.init_database(db_path)
+    if db_init_error:
+        typer.secho(
+            f"Creating database file failed with {ERRORS[db_init_error]}",
+            fg = typer.colors.RED
+        )
+        raise typer.Exit(1)
+    typer.secho(f"The to-do database is {db_path}", fg = typer.colors.GREEN)
